fa06d9ca307621b5fc36bba3ad5a089fc68f92e4
pritikalaskar <89019241+pritikalaskar@users.noreply.github.com>
2021-08-16 17:28:02 +0530

    initial changes

---
+++ Demo1Test.java
@@ -0,0 +1,16 @@
+package mavenjava.mvndemo;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test
+	public void chrome() {
+		System.out.println("chrome running");
+	}
+
+	@Test
+	public void firefox() {
+		System.out.println("Firefox Running");
+	}
+}
